557cd3181ff598250c82c546cc5164cf641a789d
saffet <saffetk502@gmail.com>
2022-02-05 14:06:17 +0100

    Zweite commit von local repository

--- src/git_project_2/FirstClass.java
+++ src/git_project_2/FirstClass.java
@@ -3,7 +3,9 @@ package git_project_2;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Erste commit von local repository");
+		System.out.println("Zweite commit von local repository");
 
 	}
 
